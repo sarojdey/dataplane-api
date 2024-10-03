19d5f3de2a5c7ae5b7baf2c5cbf9d64794e51835
SRJ <sarojkumardey.contact@gmail.com>
2024-10-03 18:27:32 +0530

    dataplane api added

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./connect");
+const authRoutes = require("./routes/auth.route.js");
+const ticketRoutes = require("./routes/ticket.route.js");
+const userRoutes = require("./routes/user.route.js");
+const commentRoutes = require("./routes/comment.route.js");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/tickets", ticketRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/comments", commentRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  connectDb();
+  console.log(`Server is running on port: ${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: "Something went wrong!" });
+});
