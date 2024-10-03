08480d724510b9f93352c7fecaa0f1514767995e
SRJ <sarojkumardey.contact@gmail.com>
2024-10-03 19:19:47 +0530

    origin all

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
